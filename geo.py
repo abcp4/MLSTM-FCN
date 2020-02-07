75dfbebd823c81db88c24945802fbe8d7f8d8ad6
Ivan Pereira <navi1921@gmail.com>
2020-02-06 22:18:57 -0300

    Update geo.py

--- geo.py
+++ geo.py
@@ -199,6 +199,6 @@ def squeeze_excite_block(input):
 if __name__ == "__main__":
     model = generate_model_2()
 
-    train_model(model, DATASET_INDEX, dataset_prefix='eeg', epochs=500, batch_size=128)
+    train_model(model, DATASET_INDEX, dataset_prefix='geo', epochs=500, batch_size=128)
 
-    evaluate_model(model, DATASET_INDEX, dataset_prefix='eeg', batch_size=128)
+    evaluate_model(model, DATASET_INDEX, dataset_prefix='geo', batch_size=128)
